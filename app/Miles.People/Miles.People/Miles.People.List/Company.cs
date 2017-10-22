cca565322fe38d41524c9615cf0151d564f92992
Peter Samuel <pjsamuel3@hotmail.com>
2017-10-22 22:15:29 +0200

    Added Miles.People.List
    A simple implementation to show proof of concept of how code can be shared between code bases

---
+++ app/Miles.People/Miles.People/Miles.People.List/Company.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Miles.People.List
+{
+    class Company
+    {
+        public Company()
+        {
+
+        }
+
+        public IEnumerable<Employee> GetEmployees()
+        {
+            return new List<Employee> {
+                new Employee { FirstName="Peter", LastName="Samuel", Title="Senior Consultant", Description="Peter needs more time to create workshop material."},
+                new Employee { FirstName="Tom", LastName="Thorsen", Title="Senior Consultant", Description=""},
+                new Employee { FirstName="Kenneth", LastName="P", Title="Senior Consultant", Description=""},
+                new Employee { FirstName="Olav", LastName="Lund Nilsen", Title="Daily slave", Description=""},
+                new Employee { FirstName="Bjørn", LastName="Nordgulen", Title="Senior Consultant", Description=""},
+            };
+        }
+    }
+}
